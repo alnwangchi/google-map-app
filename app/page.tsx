044e365978fe70cfeb70ca9d15c2b20f82ee651a
tsungyu927 <tsungyu927@gmail.com>
2023-11-05 15:44:37 +0800

    feat: [map] add default map

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
-import Marker from '@/components/Marker';
-import InformationCard from '@/components/Marker';
-import Image from 'next/image';
+'use client';
+
+import MapComponent from '@/component/MapComponent';
+import { Wrapper, Status } from '@googlemaps/react-wrapper';
+
+// Use Taipei Train Station as default center
+const DEFAULT_CENTER: google.maps.LatLngLiteral = {
+  lat: 25.048111960214577,
+  lng: 121.51705041522831
+};
+const DEFAULT_ZOOM: number = 12;
 
 export default function Home() {
+  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY || '';
+
+  console.log({ apiKey });
   return (
-    <main className="flex min-h-screen items-center justify-center p-24">
-      <Marker name="OH YA" recycleQTY={11} />
+    <main className="h-screen w-screen">
+      <Wrapper apiKey={apiKey}>
+        <MapComponent center={DEFAULT_CENTER} zoom={DEFAULT_ZOOM} />
+      </Wrapper>
     </main>
   );
 }
