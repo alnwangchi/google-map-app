984badceb737b3a8028c2f16a1b1b80d84013d9c
alnwangchi <alnwangchi@gmail.com>
2023-11-11 00:01:31 +0800

    test: cache issue

--- app/page.tsx
+++ app/page.tsx
@@ -11,15 +11,13 @@ export default function Home() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(
-          `${process.env.NEXT_PUBLIC_DOMAIN}/api/stores?random=${Math.random()}`,
-          {
-            headers: {
-              'Content-Type': 'application/json;charset=UTF-8'
-            },
-            cache: 'no-cache'
-          }
-        );
+        const response = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/api/stores`, {
+          headers: {
+            'Content-Type': 'application/json;charset=UTF-8'
+          },
+          cache: 'no-cache',
+          next: { revalidate: 10 }
+        });
 
         if (!response.ok) {
           throw new Error(`Error: ${response.status}`);
