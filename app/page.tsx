450eb7c86c052dce4cade84bd94f8c1eb309cc0b
alnwangchi <alnwangchi@gmail.com>
2023-11-10 22:58:38 +0800

    fix: type error

--- app/page.tsx
+++ app/page.tsx
@@ -4,43 +4,34 @@ import { TStores } from '@/interface/I_Stores';
 import { storesConverter } from '@/utils/converter';
 import { useEffect, useState } from 'react';
 
-// async function getStores() {
-//   const res = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/api/stores`, {
-//     headers: {
-//       'content-type': 'application/json;charset=UTF-8'
-//     },
-//     cache: 'no-store'
-//   });
-//   if (!res.ok) {
-//     // This will activate the closest `error.js` Error Boundary
-//     throw new Error('Failed to fetch data');
-//   }
-//   return await res.json();
-// }
-
 export default function Home() {
-  // const storesResponse = await getStores();
-
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<TStores[]>([]);
   console.log('🚀 ~ data:', data);
 
   useEffect(() => {
-    (() => {
-      fetch(`http://localhost:3000/api/stores`, {
-        headers: {
-          'content-type': 'application/json;charset=UTF-8'
-        },
-        cache: 'no-store'
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          setData(storesConverter(data));
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/api/stores', {
+          headers: {
+            'Content-Type': 'application/json;charset=UTF-8'
+          },
+          cache: 'no-store'
         });
-    })();
-  }, []);
 
-  // const stores = storesConverter(storesResponse);
-  const stores = [];
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status}`);
+        }
+
+        const responseData = await response.json();
+        const stores = storesConverter(responseData);
+        setData(stores as TStores[]);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <main className="h-screen w-screen">
